32b010c022704706d302e1950e1463f522853f63
Martin Mulholland <martin@evolvedsearch.co.uk>
2018-12-12 10:19:43 +0000

    first commit

--- src/client/Containers/CalculatorControler.tsx
+++ src/client/Containers/CalculatorControler.tsx
@@ -8,9 +8,12 @@ import * as objectAssign from 'object-assign';
 interface IState {
     age: number,
     cupsPW: number,
-    stage: number
+    stage: number,
+    showEmbedCode: boolean
 }
 const APIURL = `https://us-central1-evolvedapi.cloudfunctions.net/EvolvedStore/?project=CoffeeCalculator`
+const EMBEDURL = `https://mwmul.github.io/CoffeeCalcApp/index.html`;
+
 export default class CalculatorControler extends React.Component<{}, IState> {
     constructor(props: {}) {
         super(props);
@@ -18,7 +21,8 @@ export default class CalculatorControler extends React.Component<{}, IState> {
         this.state = {
             age: null,
             cupsPW: null,
-            stage: 1
+            stage: 1,
+            showEmbedCode: false
         };
     };
 
@@ -76,6 +80,14 @@ export default class CalculatorControler extends React.Component<{}, IState> {
             });
     };
 
+    public toggleEmbedCode = (): void => {
+        this.setState(prevState => {
+            return Object.assign({}, prevState, {
+                ...prevState,
+                showEmbedCode: !prevState.showEmbedCode
+            });
+        });
+    };
 
     public storeData = (data) => fetch(APIURL, {
             method: "POST",
@@ -93,46 +105,67 @@ export default class CalculatorControler extends React.Component<{}, IState> {
 
     public render(): JSX.Element {
         return (
-            <div className="CalculatorControler">
+            <>
+                <div className="CalculatorControler">
+                    <div className="top">
+                        <img src={require('../../../public/images/topLeft.jpg')} alt=""/>
+                        <img src={require('../../../public/images/topRight.jpg')} alt=""/>
+                    </div>
+                    
+                    <main className="main" role="main">
                 
-             
-                <div className="top">
-                    <img src={require('../../../public/images/topLeft.jpg')} alt=""/>
-                    <img src={require('../../../public/images/topRight.jpg')} alt=""/>
+                        {
+                            this.state.stage === 1 
+                                ?   <StageOne changeCups={this.changeCups} changeAge={this.changeAge} submitForm={this.submitForm}/>
+                                :   null
+                        }
+
+                        {
+                            this.state.stage === 2
+                                ?   <div className="spinner"></div>
+                                :   null
+                        }
+
+                        {
+                            this.state.stage === 3
+                                ?   <StageThree storeData={this.storeData} restart={this.restart} cups={this.state.cupsPW} age={this.state.age} />
+                                :   null
+                        }
+                    
+                    </main>
+
+                    <div className="bottom">
+                        {
+                            this.state.stage !== 3
+                                ?   <img src={require('../../../public/images/bottomLeft.jpg')} alt="" />
+                                :   null
+                        }
+                    </div>
+                    
+
                 </div>
-                
-                <main className="main" role="main">
-            
-                    {
-                        this.state.stage === 1 
-                            ?   <StageOne changeCups={this.changeCups} changeAge={this.changeAge} submitForm={this.submitForm}/>
-                            :   null
-                    }
-
-                    {
-                        this.state.stage === 2
-                            ?   <div className="spinner"></div>
-                            :   null
-                    }
-
-                    {
-                        this.state.stage === 3
-                            ?   <StageThree storeData={this.storeData} restart={this.restart} cups={this.state.cupsPW} age={this.state.age} />
-                            :   null
-                    }
-                
-                </main>
-
-                <div className="bottom">
-                    {
-                        this.state.stage !== 3
-                            ?   <img src={require('../../../public/images/bottomLeft.jpg')} alt="" />
-                            :   null
-                    }
+                <div className="embedCodeToggle" onClick={this.toggleEmbedCode}>
+                    <i className="fas fa-code"></i>
                 </div>
-                 
-
-            </div>
+                {
+                    this.state.showEmbedCode
+                        ? <div className="embedCode">
+                        <div className="embedHeader">
+                            <h3>Embed This App</h3> <i onClick={this.toggleEmbedCode} className="fas fa-times"></i>
+                        </div>
+                       <div className="inner">
+                            {`<div style="width: 100%; height: 500px">`}
+                                <br/>
+                                    {`<iframe style="width: 100%; height: 500px;" src="${EMBEDURL}" frameborder="0"></iframe>`}
+                                <br/>
+                            {`</div>`}
+                       </div>
+                        
+                            
+                    </div>
+                    : null
+                }
+            </>
         );
     };
 };
